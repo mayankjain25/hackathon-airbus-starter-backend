fefc10c8cb576b234458e158c81225f6f0b9fbf1
Mayank Jain <jainmayank2509@gmail.com>
2023-05-12 23:41:55 +0530

    added more filters

--- server.js
+++ server.js
@@ -8,13 +8,19 @@ const PORT = process.env.PORT || 3000;
 
 const jsonData = require('/home/mayank/Desktop/projects/aoos-hackathon/aircraft_parts_data.json');
 
-const filteredData = jsonData.filter(item => item['Recycling Rate (%)'] > 60);
+const filteredData = jsonData.filter(item => item['Recycling Rate (%)'] > 60 && item['Condition']==='Used' && item['Renewable Material Content (%)']>60
+) ;
+
+const manufPotential = jsonData.filter(item=> item['Remanufacturing Potential (%)']>60 && item['Condition']==='Used') 
 
 
 app.get('/recycled-data', (req, res) => {
     res.json(filteredData);
   });
 
+app.get('/manuf-potential', (req, res) => {
+    res.json(manufPotential);
+    });
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
   }
